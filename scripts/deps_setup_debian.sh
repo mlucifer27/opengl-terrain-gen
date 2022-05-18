bb751cc0760fd5cfbe8714b675e186effc3512ef
mlucifer27 <nexgen.luciferr27@gmail.com>
2022-05-18 15:10:15 +0200

    build: fix build env

---
+++ scripts/deps_setup_debian.sh
@@ -0,0 +1,2 @@
+sudo apt-get install libegl1-mesa-dev
+sudo apt-get install freeglut3-dev
--- src/main.cpp
+++ src/main.cpp
@@ -20,24 +20,83 @@
 
 #include <math.h>
 
+//*************************************************************************
+// Function that draws a reference system
+//*************************************************************************
+void DrawReferenceSystem()
+{
+  // set the line width to 3.0
+  glLineWidth(3.0);
+
+  // Draw three lines along the x, y, z axis to represent the reference system
+  // Use red for the x-axis, green for the y-axis and blue for the z-axis
+  glColor3f(1.0, 0.0, 0.0);
+  glBegin(GL_LINES);
+  glVertex3f(0.0, 0.0, 0.0);
+  glVertex3f(1.0, 0.0, 0.0);
+  glEnd();
+
+  glColor3f(0.0, 1.0, 0.0);
+  glBegin(GL_LINES);
+  glVertex3f(0.0, 0.0, 0.0);
+  glVertex3f(0.0, 1.0, 0.0);
+  glEnd();
+
+  glColor3f(0.0, 0.0, 1.0);
+  glBegin(GL_LINES);
+  glVertex3f(0.0, 0.0, 0.0);
+  glVertex3f(0.0, 0.0, 1.0);
+  glEnd();
+
+  // reset the drawing color to white
+  glColor3f(1.0, 1.0, 1.0);
+
+  // reset the line width to 1.0
+  glLineWidth(1.0);
+}
+
 void display()
 {
   // clear the screen with blue
   glClear(GL_COLOR_BUFFER_BIT);
 
-  // draw stuff here
+  glPushMatrix();
 
-  glFlush();
+  DrawReferenceSystem();
+
+  glPopMatrix();
+
+  glutSwapBuffers();
+}
+
+void init(void)
+{
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(65.0, 1.0, 1.0, 100.0);
+
+  glShadeModel(GL_FLAT);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  // Place the camera
+  gluLookAt(-6, 5, -6, 0, 0, 2, 0, 1, 0);
 }
 
 void reshape(int width, int height)
 {
-  /* define the viewport transformation */
+  // define the viewport transformation;
   glViewport(0, 0, width, height);
+  if (width < height)
+    glViewport(0, (height - width) / 2, width, width);
+  else
+    glViewport((width - height) / 2, 0, height, height);
 }
 
 int main(int argc, char **argv)
 {
+
   // initialize glut
   glutInit(&argc, argv);
 
@@ -53,6 +112,9 @@ int main(int argc, char **argv)
   // create the window
   glutCreateWindow("Terrain interpolation demo");
 
+  // initialize the OpenGL graphics state
+  init();
+
   // set the display callback
   glutDisplayFunc(display);
 
@@ -61,4 +123,6 @@ int main(int argc, char **argv)
 
   // enter the event loop
   glutMainLoop();
+
+  return EXIT_SUCCESS;
 }
